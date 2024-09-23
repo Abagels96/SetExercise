af6145669b42a252a70941c1c39f7d32189b4111
Abagels96 <abmen18@gmail.com>
2024-09-22 20:36:07 -0500

    next steps

---
+++ src/main/CardSharks.java
@@ -0,0 +1,24 @@
+package main;
+
+public class CardSharks {
+	
+	 private String name;
+	private String cardHand;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getCardHand(){
+		return cardHand;
+		
+	}
+	public void setCardHand(String cardHand) {
+		this.cardHand=cardHand;
+	}
+	
+	
+	
+
+}
--- src/main/SetExerciseApplication.java
+++ src/main/SetExerciseApplication.java
@@ -8,8 +8,10 @@ import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
@@ -20,67 +22,95 @@ public class SetExerciseApplication {
 
 	public static void main(String[] args) throws IOException {
 			Set<String> newPokerHands= new HashSet<String>() ;
-			String[]pokerHand=new String[44];
-		int i=0;
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader("PokerHands.csv"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			String lines="";
-			while((lines=br.readLine())!=null) {
-				System.out.println(lines);
-				
-				pokerHand =lines.split(", ");
-				
-				
-				System.out.println(Arrays.toString(pokerHand));
-				
-				for(String hand:pokerHand) {
-					Arrays.copyOf(hand);
-					System.out.print(hand);
-				newPokerHands.add(hand);
-				System.out.println(newPokerHands);
-				}
-				
-				
-			}
+	
 		
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// extract the data from the CSV done 
-		BufferedWriter writer= new BufferedWriter(new FileWriter("pokerHands2.csv"));
-		for (String hand:newPokerHands) {
-			writer.write(hand);
-			writer.newLine();
-		}
-		
-		writer.close();
-		// remove ACE HIGH and QUEEN HIGH
-		
-		
+			extractDataFromCSV("PokerHands.csv");
 		// Update PAIR to DEUCES
 		printToConsole(newPokerHands);
 		
-	}
-	
-	public static void printToConsole(Set<String>newPokerHands) {
-
-		for(String hand:newPokerHands) {
-			System.out.println(hand);
-		}
+		removeHands(newPokerHands);
+		
+		printToConsole(newPokerHands);
+			// remove ACE HIGH and QUEEN HIGH
 		
 	}
-	
-	public static Set<String> extractDataFromCSV(String fileName) {
 
-		return null;
+	private static void removeHands(Set<String> newPokerHands) {
+		for(String hand:newPokerHands) {
+			String firstHand="ACE HIGH";
+			String secondHand="QUEEN HIGH";
+			if(hand.equalsIgnoreCase(firstHand)) {
+				newPokerHands.remove(firstHand);
+			}
+			if(hand.equalsIgnoreCase(secondHand)) {
+				newPokerHands.remove(secondHand);
+			}
+		}
+	}
+	
+	
+	
+	public static Set<String> extractDataFromCSV(String fileName) throws IOException {
+		Set<String> newPokerHands= new HashSet<String>() ;
+
+		String[]pokerHand=new String[4];
+		String[]justPokerHands= new String[44];
+		CardSharks cardSharks= new CardSharks();
+	int i=0;
+	BufferedReader br = null;
+	try {
+		br = new BufferedReader(new FileReader("PokerHands.csv"));
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	try {
+		String lines="";
+		br.readLine();
+		while((lines=br.readLine())!=null) {
+			System.out.println(lines);
+			
+			pokerHand =lines.split(",");
+			
+			System.out.println(pokerHand[0]);
+		
+	
+			
+			 cardSharks.setCardHand(pokerHand[1]);
+			 
+			  System.out.println(cardSharks.getCardHand());
+			 
+		justPokerHands[i]=cardSharks.getCardHand();
+		i++;
+		System.out.println(Arrays.toString(justPokerHands));
+		
+		
+			
+			
+		}
+	for(String hand:justPokerHands) {
+			newPokerHands.add(hand);
+			
+		}
+System.out.println(newPokerHands);
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	// extract the data from the CSV done 
+	
+	
+
+		return newPokerHands;
 	}
 
+public static void printToConsole(Set<String>newPokerHands) throws IOException {
+
+for(String hand:newPokerHands) {
+	System.out.println(hand);
+}
+
+		
+	}
 }
